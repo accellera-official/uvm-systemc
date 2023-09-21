4ef6679dfd3c1dee0b279983605c7d051123dde2
Bas Arts <bas.arts@nxp.com>
2023-09-21 09:17:53 +0200

    Added comment regarding uvm-core code

--- src/uvmsc/reg/uvm_reg_map.cpp
+++ src/uvmsc/reg/uvm_reg_map.cpp
@@ -1202,6 +1202,7 @@ void uvm_reg_map::do_bus_write( uvm_reg_item* rw,
       rw_access.n_bits = (n_bits > bus_width*8) ? bus_width*8 : n_bits;
       rw_access.byte_en = byte_en;
 
+      // TODO: this code differs from SystemVerilog (github #233)
       if (rw_access.kind == UVM_WRITE && m_endian == UVM_BIG_ENDIAN) {
         uvm_reg_data_t temp {0};
         for (unsigned i = 0; i < bus_width; ++i) {
@@ -1392,6 +1393,7 @@ void uvm_reg_map::do_bus_read( uvm_reg_item* rw,
       else
         adapter->bus2reg(bus_req, rw_access);
 
+      // TODO: this code differs from SystemVerilog (github #233)
       if (rw_access.kind == UVM_READ && m_endian == UVM_BIG_ENDIAN) {
         uvm_reg_data_t temp {0};
         for (unsigned i = 0; i < bus_width; ++i) {
